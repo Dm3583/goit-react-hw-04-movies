d68a5961f26e445be2303c20b3630daa79b58508
Dm3583 <pankovdm@gmail.com>
2021-03-22 20:18:28 +0200

    add suspense, add lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import styles from './App.module.css';
 import AppBar from './AppBar';
-import HomePage from '../views/HomePage';
-import MoviesPage from '../views/MoviesPage';
-import MovieDetailsPage from '../views/MovieDetailsPage';
 import routes from '../routes';
+
+const HomePage = lazy(() =>
+  import('../views/HomePage' /* webpackChunkName: "home-page" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    '../views/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */
+  ),
+);
+const MoviesPage = lazy(() =>
+  import('../views/MoviesPage' /* webpackChunkName: "movies-page" */),
+);
 
 function App() {
   return (
     <div className={styles.App}>
       <AppBar />
-      <Switch>
-        <Route path={routes.home} exact component={HomePage} />
-        <Route
-          path={routes.movieDetails}
-          render={props => <MovieDetailsPage {...props} />}
-        />
-        <Route path={routes.movies} component={MoviesPage} />
-        <Redirect to={routes.home} />
-      </Switch>
+      <Suspense fallback={<h1>Loading ...</h1>}>
+        <Switch>
+          <Route path={routes.home} exact component={HomePage} />
+          <Route
+            path={routes.movieDetails}
+            render={props => <MovieDetailsPage {...props} />}
+          />
+          <Route path={routes.movies} component={MoviesPage} />
+          <Redirect to={routes.home} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './MoviesList.module.css';
+import routes from '../../routes';
 
 const MoviesList = ({ films, location }) => (
   <div>
@@ -10,7 +11,7 @@ const MoviesList = ({ films, location }) => (
         <li key={id}>
           <NavLink
             to={{
-              pathname: `/movies/${id}`,
+              pathname: `${routes.movies}/${id}`,
               state: {
                 from: location,
               },
